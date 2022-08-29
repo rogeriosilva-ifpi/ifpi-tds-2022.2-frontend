e7b834162264a0df4e3442f74207759c519b9670
Rogério Silva <64442407+rogeriosilva-ifpi@users.noreply.github.com>
2022-08-29 18:16:43 -0300

    Básico HTML e CSS

---
+++ copa2022/assets/js/comportamento.js
@@ -0,0 +1,11 @@
+function app(){
+
+    const input_nome = document.getElementById('input_nome');
+
+    const btn_button = document.getElementById('btn_saudacao');
+
+    btn_button.onclick = () => alert('oi');
+
+}
+
+app()
