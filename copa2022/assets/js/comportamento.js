b77a9e9cb481496e3100a1957c35814716dafa61
Rogério Silva <64442407+rogeriosilva-ifpi@users.noreply.github.com>
2022-08-30 10:34:45 -0300

    Atualização saudacao no Copa22

--- copa2022/assets/js/comportamento.js
+++ copa2022/assets/js/comportamento.js
@@ -1,11 +1,35 @@
 function app(){
 
-    const input_nome = document.getElementById('input_nome');
+    console.log('#### APP Started ####')
+    print('Rogério')
 
-    const btn_button = document.getElementById('btn_saudacao');
+    const cx_texto_nome = document.getElementById('input_nome')
+    const botao_saudacao = document.getElementById('btn_saudacao')
+    const texto_saudacao = document.getElementById('texto_saudacao')
 
-    btn_button.onclick = () => alert('oi');
+    /*
+    console.log(cx_texto_nome)
 
+    console.log(cx_texto_nome.placeholder)
+    console.log(cx_texto_nome.id)
+    cx_texto_nome.placeholder = 'Digite seu apelido!'
+
+    alert(botao_saudacao.innerText)
+    botao_saudacao.innerText = 'Hello'
+    */
+    
+    // Programar um evento click no botao
+    botao_saudacao.addEventListener('click', () => {
+        const texto_digitado = cx_texto_nome.value
+        
+        // alert(texto_digitado)
+        texto_saudacao.innerText = texto_digitado
+    })
+
+}
+
+function print(texto){
+    alert(texto)
 }
 
 app()
